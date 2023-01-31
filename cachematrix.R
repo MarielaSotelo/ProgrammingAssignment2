b3512a1a8b4bc6f7cc9f4d9d8c6e5dfecab2b477
MarielaSotelo <116769288+MarielaSotelo@users.noreply.github.com>
2023-01-30 19:06:29 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hay dos funciones para poder generar la inversa de una matriz:
+## makeCacheMatrix y cacheSolve
 
-## Write a short comment describing this function
-
+## Esta funcion genera la matriz de la cual se obtendrá la inversa
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversa<-NULL
+  set<-function(y){
+    x<<-y
+    inversa<<-NULL
+  }
+  get<-function() x
+  setinversa<-function(inverse) inversa<<-inverse
+  getinversa<-function()
+  {
+    inver<-ginv(x)
+    inver%*%x
+  } 
+  
+  list(set=set,get=get,setinversa=setinversa,getinversa=getinversa)
 }
 
 
-## Write a short comment describing this function
+## Esta función regresa la matriz inversa
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inversa<-x$getinversa()
+  if(!is.null(inversa)){
+    message("Obteniendo matriz")
+    return(inversa)
+  }
+  matriz<-x$get()
+  inversa<-solve(matriz,...)
+  x$setinversa(inversa)
+  inversa
 }
